b82bc13f84e76f564503212205a3a686ccaa09d1
linebutcher <dlxodid1115@gmail.com>
2024-01-02 16:41:43 +0900

    백준 사칙연산 3 (a-b)

--- sc/src/number1/Main.java
+++ sc/src/number1/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt(), b=scanner.nextInt();
-        System.out.println(a+b);
+        System.out.println(a-b);
     }
 }
