cdc53fde726b8430eb3d88dd229d789b27bb97a2
linebutcher <dlxodid1115@gmail.com>
2024-01-03 08:25:58 +0900

    백준 사칙연산 5(a/b)2

--- sc/src/number1/Main.java
+++ sc/src/number1/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt(), b=scanner.nextInt();
+        double a = scanner.nextLong(), b=scanner.nextLong();
         System.out.println(a/b);
     }
 }
